7931198f991ac9a913b659fad2740b260f38fae4
WolfEYc <isaacwolfsite@gmail.com>
2021-03-03 14:20:57 -0600

    numConverter upgrade

--- numConvert/numConverter.cpp
+++ numConvert/numConverter.cpp
@@ -10,7 +10,6 @@ int letNum(char letter)
     else
         return letter - 48;
 }
-
 char numLet(int num)
 {
     if (num < 10)
@@ -18,7 +17,6 @@ char numLet(int num)
     else
         return num + 55;
 }
-
 string hexToDec(string hex)
 {
     int num = 0;
@@ -26,11 +24,10 @@ string hexToDec(string hex)
         num += (letNum(hex[i]) * pow(16, hex.length() - i - 1));
     return to_string(num);
 }
-
 string decToHex(string dec)
 {
     string out = "";
-    long long num = stoll(dec);    
+    long long num = stoll(dec);
     if(num<0)
         num += 4294967296;    
     while (num > 0) {
@@ -39,46 +36,91 @@ string decToHex(string dec)
     }
     return out;
 }
-
 string decToBinary(string dec){
-    int num = stoi(dec);
-    string out = "";    
+    long long num = stoll(dec);
+    string out = "";  
+    if(num<0){
+        string bean = decToBinary(to_string(-1*num));
+        if(bean.length()>16){
+            out = "11111111111111111111111111111111";
+        }else if(bean.length()>8){
+            out = "1111111111111111";
+        }else{
+            out = "11111111";
+        }        
+        for(int i=0;i<bean.length();i++)
+            if(bean[i]=='1')
+                out[out.length()-bean.length()+i]='0';
+        for(int i=out.length();i>0;i--){
+            if(out[i]=='0'){
+                out[i]='1';
+                break;
+            }
+            out[i]='0';
+        }
+        return out;
+    }   
     while (num > 0) {
         out = to_string(num % 2) + out;
         num /= 2;
-    }
+    }    
     return out;
 }
-
 string biToDec(string bi){
-    int num = 0;
-    for(int i=bi.length()-1,c=1;i>=0;i--,c*=2)      
-        num+=letNum(bi[i])*c;   
-    return to_string(num);
-}
-
-int main(int argc, char **argv){
-    if (argc != 4 || (string(argv[1]) != "hex" && string(argv[1]) != "dec" && string(argv[1]) != "bi")) {
-        cout << "invalid input"<<endl;
-        return 0;
+    for(int i = 0; i<bi.length();i++)
+        if(bi[i]!='0' && bi[i]!='1')
+            return "invalid number";
+    bool neg = 0;    
+    if(bi.length()%8==0 && bi[0]=='1' && bi.length()!=24){
+        neg=1;
+        int i;
+        while(bi.length()>0){
+            if(bi[0]=='0')
+                break;
+            bi=bi.substr(1);
+        }
+        for(i = 0;i<bi.length();i++)
+            if(bi[i]=='0')
+                bi[i]='1';
+            else
+                bi[i]='0';          
+        for(i=bi.length()-1;i>=0;i--){
+            if(bi[i]=='0'){
+                bi[i]='1';
+                i = -2;
+                break;
+            }
+            bi[i]='0';
+        }        
+        if(i!=-2)
+            bi = '1'+bi;                
     }
-    string from = argv[1];
-    string to = argv[2];
-    string num = argv[3];    
-    if (from == "hex")
-        if(to=="dec")
-            cout << hexToDec(num);
-        else
-            cout<< decToBinary(hexToDec(num));
-    if (from == "dec")
-        if(to=="hex")
-            cout << decToHex(num);
-        else
-            cout<< decToBinary(num);
-    if(from == "bi")
-        if(to=="dec")
-            cout << biToDec(num);
-        else
-            cout << decToHex(biToDec(num));
-    cout << endl; 
+    long long num = 0;
+    long long c = 1;
+    for(int i=bi.length()-1;i>=0;i--,c*=2)      
+        num+=letNum(bi[i])*c;   
+    return (neg==0) ? to_string(num) : "-"+to_string(num);
+}
+int main(int argc, char **argv){
+    try{
+        string from = argv[1],to = argv[2], num = argv[3];    
+        if (from == "hex")
+            if(to=="dec")
+                cout << hexToDec(num);
+            else
+                cout<< decToBinary(hexToDec(num));
+        if (from == "dec")
+            if(to=="hex")
+                cout << decToHex(num);
+            else
+                cout<< decToBinary(num);
+        if(from == "bi")
+            if(to=="dec")
+                cout << biToDec(num);
+            else
+                cout << decToHex(biToDec(num));
+    }catch(...){
+        cout << "./main (from) (to) (number)";
+    }
+    cout << endl;
 }
